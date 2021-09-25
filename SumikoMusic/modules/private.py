d4db0586fbed3e9b26d1ebfc57f7e89e1f9a2146
PRINCE RAJ <87780825+TheCyberKingop@users.noreply.github.com>
2021-09-25 22:58:29 +0530

    Update private.py

--- SumikoMusic/modules/private.py
+++ SumikoMusic/modules/private.py
@@ -1,47 +1,125 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+# SumikoMusic (Telegram bot project )
+# Copyright (C) 2021  Inukaasith
 
-from config import BOT_NAME as bn
-from helpers.filters import other_filters2
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-@Client.on_message(other_filters2)
-async def start(_, message: Message):
-      await message.reply_text(
-        """𝐇𝐞𝐲, 𝐈'𝐦 𝐕𝐜 𝐁𝐨𝐭❤️🔥. 
-𝐈 𝐂𝐚𝐧 𝐏𝐥𝐚𝐲 𝐌𝐮𝐬𝐢𝐜 𝐈𝐧 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 𝐕𝐨𝐢𝐜𝐞 𝐂𝐡𝐚𝐭.
-𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 𝐀𝐧𝐝 𝐏𝐥𝐚𝐲 𝐌𝐮𝐬𝐢𝐜 𝐅𝐫𝐞𝐞𝐥𝐲! 
-/help - 𝐓𝐨 𝐆𝐞𝐭 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬.✅""",
+import logging
+from SumikoMusic.modules.msg import Messages as tr
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton
+from pyrogram.types import Messages
+from SumikoMusic.config import ASSISTANT_NAME
+from SumikoMusic.config import PROJECT_NAME
+from SumikoMusic.config import SUPPORT_GROUP
+from SumikoMusic.config import UPDATES_CHANNEL
+from SumikoMusic.config import BOT_USERNAME
+logging.basicConfig(level=logging.INFO)
+
+@Client.on_message(filters.private & filters.incoming & filters.command(['start']))
+def _start(client, message):
+    client.send_message(message.chat.id,
+        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
+        parse_mode="markdown",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🎶Oᴡɴᴇʀ❤🔥", url="https://t.me/itz_cyberking_xd")
-                  ],[
+                        "★ Add me to your Group ★", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+                [
                     InlineKeyboardButton(
-                        "🔥ßƐSŦĪƐS ZᎾИƐ🔥", url="https://t.me/BONDOFBESTIZZ"
-                    ),
+                        "✰Updates✰", url=f"https://t.me/{UPDATES_CHANNEL}"), 
                     InlineKeyboardButton(
-                        "🔮 Channel 🔮", url="https://t.me/INCREDIBLE_SPAM_BOT"
-                    )    
-                ],[ 
+                        "🔥ßƐSŦĪƐS ZᎾИƐ🔥", url=f"https://t.me/{SUPPORT_GROUP}")
+                ],[
                     InlineKeyboardButton(
-                        "➕Aᴅᴅ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ➕", url="https://t.me/DEVILXPRINCE_ROBOT?startgroup=true"
-                    )]
+                        "🎶Oᴡɴᴇʀ❤🔥", url=f"https://t.me/itz_cyberking_xd")
+                ]
             ]
         ),
-     disable_web_page_preview=True
-    )
+        reply_to_message_id=message.message_id
+        )
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**Yes iᴍ online ✅**""",
-      reply_markup=InlineKeyboardMarkup(
+    await message.reply_text(
+        f"""**🔴 {PROJECT_NAME} is online**""",
+        reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🔥ßƐSŦĪƐS ZᎾИƐ🔥", url="https://t.me/BONDOFBESTIZZ")
+                        "💬 Support Chat", url=f"https://t.me/{SUPPORT_GROUP}"
+                    )
                 ]
             ]
-        )
-   )
+        ),
+    )
+
+
+@Client.on_message(filters.private & filters.incoming & filters.command(['help']))
+def _help(client, message):
+    client.send_message(chat_id = message.chat.id,
+        text = tr.HELP_MSG[1],
+        parse_mode="markdown",
+        V('help+'))
+
+@Client.on_callback_query(help_callback_filter)
+def help_answer(client, callback_query):
+    chat_id = callback_query.from_user.id
+    disable_web_page_preview=True
+    message_id = callback_query.message.message_id
+    msg = int(callback_query.data.split('+')[1])
+    client.edit_message_text(chat_id=chat_id,    message_id=message_id,
+        text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(map(msg))
+    )
+
+
+def map(pos):
+    if(pos==1):
+        button = [
+            [InlineKeyboardButton(text = '▶️', callback_data = "help+2")]
+        ]
+    elif(pos==len(tr.HELP_MSG)-1):
+        url = f"https://t.me/{SUPPORT_GROUP}"
+        button = [
+            [InlineKeyboardButton("➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+            [InlineKeyboardButton(text = '📲 Updates', url=f"https://t.me/{UPDATES_CHANNEL}"),
+             InlineKeyboardButton(text = '💬 Support', url=f"https://t.me/{SUPPORT_GROUP}")],
+            [InlineKeyboardButton(text = '🎶Oᴡɴᴇʀ❤🔥', url=f"https://t.me/itz_cyberking_xd")],
+            [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
+        ]
+    else:
+        button = [
+            [
+                InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}"),
+                InlineKeyboardButton(text = '▶️', callback_data = f"help+{pos+1}")
+            ],
+        ]
+    return button
+
+@Client.on_message(filters.command("help") & ~filters.private & ~filters.channel)
+async def ghelp(_, message: Message):
+    await message.reply_text(
+        f"""**🙋‍♀️ Hello there! I can play music in the voice chats of telegram groups & channels.**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🟡 Click here for help 🟡", url=f"https://t.me/{BOT_USERNAME}?start"
+                    )
+                ]
+            ]
+        ),
+    )
